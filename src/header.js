ce633bd1c1f9349a7cb3fccffe15fa43664298d8
Engelbert Niehaus <niehaus@uni-landau.de>
2018-05-06 07:56:18 +0200

    First working build.sh updated and bugfix in LinkParam pVars instead of pVarHash. Create a build.js for platform independency still necessary

---
+++ src/header.js
@@ -0,0 +1,18 @@
+// NPM Package Header:  LinkParam()
+// Repository: https://www.github.com/niebert/LinkParam
+// LinkParam can be used with multiple instances
+// e.g. for iframes, init the instance with the "document" instance.
+// see example
+/*
+
+const LinkParam = require('linkparam');
+var vLinkParam = new LinkParam();
+vLinkParam.init(document);
+if (vLinkParam.exists("myparam")) {
+  var vPar = vLinkParam.getValue("myparam");
+  alert("myparam="+vPar);
+} else {
+  alert("HTML file needs a LinkParameter 'myparam'")
+}
+
+*/
